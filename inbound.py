cd4c54f67df0f3b38928ee9134f70022ba650773
Sankruth Kota <sankruth.kota@gmail.com>
2017-07-14 13:12:30 -0400

    got rid of some account tokens  and other info and added the ngrok terminal for use

--- inbound.py
+++ inbound.py
@@ -1,18 +1,32 @@
 from flask import Flask, request
 import json
-from twilio import twiml
+from twilio.twiml.messaging_response import MessagingResponse
+import requests
 
-account_sid = "XXXXXXXXX"
-auth_token = "XXXXXXXX"
-accu_key = "XXXXXXX" 
+
+accu_key = "HIDDEN" 
 
 app = Flask(__name__)
 @app.route('/sms', methods=['POST', 'GET'])
 
-
-
-def sms():
+def weather():
+	
 	message_body = request.form['Body']
-	resp = twiml.Response()
+	url = 'http://dataservice.accuweather.com/forecasts/v1/daily/1day/{}?apikey=%091{}&language=en-us&details=false&metric=false'.format(message_body, accu_key)
 
-	resp.message('')
+	d = requests.get(url = url)
+
+	data = d.json()
+
+	minimum = data['DailyForecasts'][0]['Temperature']['Minimum']['Value']
+	maximum = data['DailyForecasts'][0]['Temperature']['Maximum']['Value']
+	day = data['DailyForecasts'][0]['Day']['IconPhrase']
+	night = data['DailyForecasts'][0]['Night']['IconPhrase']
+
+	
+	resp = MessagingResponse()
+	resp.message("Here's the weather for {}, with all tempartures in Fahrenheit: \n Minimum:{} \n Maximum:{} \n Day:{} \n Night:{}".format(message_body, minimum, maximum, day, night))
+	return str(resp)
+
+if __name__=="__main__":
+	app.run(debug=True)
