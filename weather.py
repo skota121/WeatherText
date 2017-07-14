fae88d4357664f3cce320d9bcf7d678b3993754d
Sankruth Kota <sankruth.kota@gmail.com>
2017-07-14 12:03:00 -0400

    added a bunch more

--- weather.py
+++ weather.py
@@ -1,1 +1,23 @@
 from twilio.rest import Client
+from flask import Flask, request, redirect
+from twilio.twiml.messaging_response import MessagingResponse
+import requests
+import json
+
+account_sid = "XXXXXXXXX"
+auth_token = "XXXXXXXX"
+accu_key = "XXXXXXX" 
+app = Flask(__name__)
+@app.route("/", methods = ['GET', 'POST'])
+
+
+
+def weather():
+	# Get weather from Accuweather API 
+	resp = MessagingResponse().message()
+	return str(resp)
+client = Client(account_sid, auth_token)
+message = client.api.account.messages.create(to = "+XXXXXXX", from_="+XXXXX", body="")
+
+if __name__ = "__main__":
+	app.run(debug=True)
