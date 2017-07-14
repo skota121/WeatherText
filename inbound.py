d697bd0b91562eda19287222af9da2004d94898a
Sankruth Kota <sankruth.kota@gmail.com>
2017-07-14 12:14:57 -0400

    added account "keys"

--- inbound.py
+++ inbound.py
@@ -2,8 +2,14 @@ from flask import Flask, request
 import json
 from twilio import twiml
 
+account_sid = "XXXXXXXXX"
+auth_token = "XXXXXXXX"
+accu_key = "XXXXXXX" 
+
 app = Flask(__name__)
 @app.route('/sms', methods=['POST', 'GET'])
+
+
 
 def sms():
 	message_body = request.form['Body']
